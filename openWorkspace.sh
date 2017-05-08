9fe90635b74c9ba9d1d9d6b493397e9569d44f8e
xavier petit <nuxion@gmail.com>
2017-05-08 15:26:47 -0300

    modify vars path and execution permmission of the script

--- openWorkspace.sh
+++ openWorkspace.sh
@@ -5,7 +5,7 @@
 # VARS #
 home="/home/nuxion"
 workspace=$1
-pathConfigs=$home"/scripts"
+pathConfigs=$home"/Projects/scripts/i3wm_workspace"
 fullpath=$pathConfigs/$workspace.props
 
 
@@ -13,7 +13,7 @@ fullpath=$pathConfigs/$workspace.props
 openApps () {
     # with this function, we are to list all apps
     # to prepare for send to i3-msg command 
-    for app in `cat $fullpath | grep app | awk -F ":" '{print $2}'`
+    for app in `cat $fullpath | grep app | awk -F "=" '{print $2}'`
     do 
         echo $line
 	i3-msg "workspace $workspace; exec $app"
@@ -23,7 +23,7 @@ openApps () {
 openLayout () {
    # take from workspace properties file, i
    # the workspace and layout to open
-   layout=`cat $fullpath | grep layoutName | awk -F ":" '{print $2}'`
+   layout=`cat $fullpath | grep layoutName | awk -F "=" '{print $2}'`
    i3-msg  "workspace $workspace; append_layout $layout"
 }
 ## MAIN () ##
@@ -32,7 +32,7 @@ if [ ! -r $fullpath ]; then exit 1; fi
 #echo $fullpath # debug
 
 # $workspace var its reused
-workspace=`cat $fullpath | grep workspaceName | awk -F ":" '{print $2}'` 
+workspace=`cat $fullpath | grep workspaceName | awk -F "=" '{print $2}'` 
 
 # then, firts open layout, and after 
 # exec every app in the config file
